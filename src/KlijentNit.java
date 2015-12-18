d22ecf8a6573632ecb4498fb0eca1462c5523b89
dulerad94 <dulerad94@gmail.com>
2015-12-18 16:39:13 +0100

    Malo izmena

--- src/KlijentNit.java
+++ src/KlijentNit.java
@@ -22,16 +22,12 @@ public class KlijentNit implements Runnable {
 	public void run() {		
 		try {
 			String odabrano=(String) klijent.getComboBox().getSelectedItem();
-			System.out.println(odabrano);
-			System.out.println(klijent.izlazniTok.toString());
-			System.out.println(klijent.ulazniTok.toString());
 			klijent.izlazniTok.println(odabrano);
-			System.out.println(odabrano);
+			
 			String odgovor=klijent.ulazniTok.readLine();
-			System.out.println(odgovor);
-			System.out.println("MNOGO JEBE OVO ZNAS");
+			
 			if(!odgovor.equals("moze")) return;	
-			System.out.println("MNOGO JEBE OVO ZNAS");
+			
 			izvrsiOperaciju();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
